4b27e4ad413dc5c7eb2119edd5a6440aabac617f
Caio Barilli <caiobarilli@gmail.com>
2023-11-21 06:44:41 -0300

    feat: add anglar material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [AppComponent, PostCreateComponent],
   bootstrap: [AppComponent],
 })
