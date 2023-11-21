7fe70d951dbf57b9d2dc19168f46ae91b32a42f5
Caio Barilli <caiobarilli@gmail.com>
2023-11-21 07:32:20 -0300

    feat: add input material & card material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   declarations: [AppComponent, PostCreateComponent],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/posts/post-create/post-create.component.ts
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'post-create',
+  styles: [
+    `
+      .mat-card {
+        max-width: 80%;
+        margin: 1rem auto;
+      }
+    `,
+  ],
   templateUrl: './post-create.component.html',
 })
 export class PostCreateComponent {
